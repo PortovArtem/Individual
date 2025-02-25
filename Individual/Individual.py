cd57a3d6275f8d7b0ca030823e75066594289648
Artem <portovartem2006@gmail.com>
2025-02-25 11:59:13 +0300

    Реализация функции с генерацией списка случайных чисел

--- Individual/Individual.py
+++ Individual/Individual.py
@@ -1,12 +1,12 @@
 import random
 
 def generate_random_list(count, min_val=5, max_val=1000):
-    """√енерирует список случайных чисел в заданном диапазоне."""
+    """Generate a list of random numbers within the specified range."""
     return [random.randint(min_val, max_val) for _ in range(count)]
 
-if __name__ == "__main__":
-    count = 10 + 10  # 10 (є по списку) + 10
-    random_numbers = generate_random_list(count)
+count = 10 + 10  # 10 (number in the list) + 10
+random_numbers = generate_random_list(count)
 
-    print("—генерированные числа:")
-    print(random_numbers)
+print("Generated numbers:")
+print(random_numbers)
+
